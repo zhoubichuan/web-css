eca01401dc7362cfc9aae718bdf7667376e83b5e
周必川 <zhoubichuan@icloud.com>
2023-07-23 15:44:44 +0800

    update

--- src/.vuepress/nav.js
+++ src/.vuepress/nav.js
@@ -1,25 +1,25 @@
 const nav = [
-    {
-        text: '基础知识',
-        items: [
-            {
-                text: '一.html',
-                link: '/1.base/1.html/1.index'
-            },
-            {
-                text: '二.css',
-                link: '/1.base/2.css/1.index'
-            },
-        ]
-    },
-    {
-        text: '进阶知识',
-        items: [
-            {
-                text: '一.h5',
-                link: '/2.advanced/1.h5/1.index'
-            },
-        ]
-    },
-]
-module.exports = nav
+  {
+    text: "基础知识",
+    items: [
+      {
+        text: "一.html",
+        link: "/1.base/1.html/1.index",
+      },
+      {
+        text: "二.css",
+        link: "/1.base/2.css/1.index",
+      },
+    ],
+  },
+  {
+    text: "进阶知识",
+    items: [
+      {
+        text: "一.h5",
+        link: "/2.advanced/1.h5/1.index",
+      },
+    ],
+  },
+];
+module.exports = nav;
